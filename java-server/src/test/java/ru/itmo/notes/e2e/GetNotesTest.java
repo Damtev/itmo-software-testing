2dc33c5dce2e5a0b5ff5a5828ce1a956dfd739c1
Damtev <damtev@yandex.ru>
2021-01-25 00:59:35 +0300

    Changed absolute database path to project relative

--- java-server/src/test/java/ru/itmo/notes/e2e/GetNotesTest.java
+++ java-server/src/test/java/ru/itmo/notes/e2e/GetNotesTest.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 
-import static com.codeborne.selenide.CollectionCondition.size;
 import static com.codeborne.selenide.CollectionCondition.sizeGreaterThanOrEqual;
 import static com.codeborne.selenide.Selenide.*;
 
